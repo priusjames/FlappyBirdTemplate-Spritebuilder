4825f4795e3b2c253c11b7d05a25a2b9ff18f0aa
James D <jdoyle@Js-MacBook-Pro.local>
2015-06-11 17:58:15 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -5,12 +5,13 @@
 @implementation GamePlayScene
 
 - (void)initialize
-[self addObstacle];
+
 
 {
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
